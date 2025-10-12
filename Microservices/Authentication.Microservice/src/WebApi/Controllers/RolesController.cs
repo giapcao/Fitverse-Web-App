da4d3ca1608748d46a80f1836b651c50f926047d
Giap Cao <giap.caodinh711@gmail.com>
2025-10-12 16:31:45 +0700

    fix authorize

--- Microservices/Authentication.Microservice/src/WebApi/Controllers/RolesController.cs
+++ Microservices/Authentication.Microservice/src/WebApi/Controllers/RolesController.cs
@@ -4,7 +4,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Application.Features;
 using Application.Roles.Command;
-using Domain.Enums;
 using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -14,7 +13,7 @@ using SharedLibrary.Common;
 
 namespace WebApi.Controllers;
 
-[Authorize(Policy = "IsAdmin")]
+[Authorize]
 [ApiController]
 [ApiVersion("1.0")]
 [Route("api/[controller]/v{version:apiVersion}")]
@@ -23,7 +22,7 @@ public class RolesController : ApiController
     public RolesController(IMediator mediator) : base(mediator)
     {
     }
-
+    [Authorize(Policy = "IsAdmin")]
     [HttpGet]
     [ProducesResponseType(typeof(IEnumerable<RoleDto>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetRoles(CancellationToken ct)
@@ -51,6 +50,7 @@ public class RolesController : ApiController
         return Ok(result.Value);
     }
 
+    [Authorize(Policy = "IsAdmin")]
     [HttpPost]
     [ProducesResponseType(typeof(RoleDto), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -66,6 +66,7 @@ public class RolesController : ApiController
         return CreatedAtAction(nameof(GetRoleById), new { id = result.Value.Id, version }, result.Value);
     }
 
+    [Authorize(Policy = "IsAdmin")]
     [HttpPut("{id:guid}")]
     [ProducesResponseType(typeof(RoleDto), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -82,34 +83,7 @@ public class RolesController : ApiController
         return Ok(result.Value);
     }
 
-    [HttpPost("users/{userId:guid}/customer")]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public Task<IActionResult> AddCustomerRole(Guid userId, CancellationToken ct) =>
-        AssignRoleToUser(userId, RoleType.Customer, ct);
-
-    [HttpPost("users/{userId:guid}/coach")]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public Task<IActionResult> AddCoachRole(Guid userId, CancellationToken ct) =>
-        AssignRoleToUser(userId, RoleType.Coach, ct);
-
-    [HttpPost("users/{userId:guid}/admin")]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public Task<IActionResult> AddAdminRole(Guid userId, CancellationToken ct) =>
-        AssignRoleToUser(userId, RoleType.Admin, ct);
-
-    [HttpPost("users/{userId:guid}/support")]
-    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public Task<IActionResult> AddSupportRole(Guid userId, CancellationToken ct) =>
-        AssignRoleToUser(userId, RoleType.Support, ct);
-
+    [Authorize(Policy = "IsAdmin")]
     [HttpDelete("{id:guid}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -123,15 +97,5 @@ public class RolesController : ApiController
 
         return NoContent();
     }
-
-    private async Task<IActionResult> AssignRoleToUser(Guid userId, RoleType role, CancellationToken ct)
-    {
-        var result = await _mediator.Send(new AssignRoleToUserCommand(userId, role), ct);
-        if (result.IsFailure)
-        {
-            return HandleFailure(result);
-        }
-
-        return Ok(result.Value);
-    }
 }
+
